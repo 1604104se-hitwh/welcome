cf1a45f2514ffdf4b375e01e429dc9a97421ef92
cn-chenium <chenium@hotmail.com>
2019-04-07 09:49:57 +0800

    Login Bug Fixed
    - 登录时使用first()取结果首个对象之后，后方不需$res_obj_array[0];
    - $res_obj_array 改名为 $res_obj;

--- Welcome/app/Http/Controllers/LoginController.php
+++ Welcome/app/Http/Controllers/LoginController.php
@@ -26,23 +26,23 @@ class LoginController extends Controller
             $stu_eid = $request->input("examId", "default");
             $stu_cid = $request->input("perId", "default");
             // $this->idValidator = new IdValidator();
-            $res_obj_array = Student::where([
+            $res_obj = Student::where([
                 ["stu_cid",$stu_cid],
                 ["stu_eid",$stu_eid],
             ])->first();
             /* 判断该名新生是否存在 */
-            if ($res_obj_array) {
+            if ($res_obj) {
                 session([
-                    "id" => $res_obj_array[0]->id,
-                    "stu_status" => $res_obj_array[0]->stu_status,
-                    "stu_num" => $res_obj_array[0]->stu_num,
-                    "stu_name" => $res_obj_array[0]->stu_name,
-                    "stu_gen" => $res_obj_array[0]->stu_gen,
-                    "stu_cid" => $res_obj_array[0]->stu_cid,
-                    "stu_eid" => $res_obj_array[0]->stu_eid,
-                    "class_id" => $res_obj_array[0]->class_id,
-                    "stu_dorm_str" => $res_obj_array[0]->stu_dorm_str,
-                    "stu_from_school" => $res_obj_array[0]->stu_from_school,
+                    "id" => $res_obj->id,
+                    "stu_status" => $res_obj->stu_status,
+                    "stu_num" => $res_obj->stu_num,
+                    "stu_name" => $res_obj->stu_name,
+                    "stu_gen" => $res_obj->stu_gen,
+                    "stu_cid" => $res_obj->stu_cid,
+                    "stu_eid" => $res_obj->stu_eid,
+                    "class_id" => $res_obj->class_id,
+                    "stu_dorm_str" => $res_obj->stu_dorm_str,
+                    "stu_from_school" => $res_obj->stu_from_school,
                     "Auth" => "new",
                 ]);
 
@@ -52,20 +52,20 @@ class LoginController extends Controller
         } else if ($loginType === "old") { // 老生部分
             $name = $request->input("name", "default");
             $perId = $request->input("perId", "default");
-            $res_obj_array = Student::where([
+            $res_obj = Student::where([
                 ["stu_name",$name],
                 ["perId",$perId],
             ])->first();
-            if ($res_obj_array) {
+            if ($res_obj) {
                 session([
-                    "id" => $res_obj_array[0]->id,
+                    "id" => $res_obj->id,
                     "stu_name" => $name,
-                    "stu_gen" => $res_obj_array[0]->stu_gen,
+                    "stu_gen" => $res_obj->stu_gen,
                     "stu_cid" => $perId,
-                    "stu_eid" => $res_obj_array[0]->stu_eid,
-                    "class_id" => $res_obj_array[0]->class_id,
-                    "stu_dorm_str" => $res_obj_array[0]->stu_dorm_str,
-                    "stu_from_school" => $res_obj_array[0]->stu_from_school,
+                    "stu_eid" => $res_obj->stu_eid,
+                    "class_id" => $res_obj->class_id,
+                    "stu_dorm_str" => $res_obj->stu_dorm_str,
+                    "stu_from_school" => $res_obj->stu_from_school,
                     "Auth" => "old",
                 ]);
                 return redirect()->intended("/senior");
@@ -73,13 +73,13 @@ class LoginController extends Controller
         } else if ($loginType === "admin") { // 管理员部分
             $userId = $request->input("userId", "default");
             $psw = $request->input("psw", "default");
-            $res_obj_array = Student::where([
+            $res_obj = Student::where([
                 ["adm_name",$userId],
                 ["adm_password",$psw],
             ])->first();
-            if ($res_obj_array) {
+            if ($res_obj) {
                 session([
-                    "id" => $res_obj_array[0]->id,
+                    "id" => $res_obj->id,
                     "name" => $userId,
                     "Auth" => "admin",
                 ]);
